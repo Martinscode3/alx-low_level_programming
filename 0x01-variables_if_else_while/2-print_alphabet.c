0c7f3dce46fe97164d9932642449acb0da5e5a68
Martins Okeke <martinokeke@rocketmail.com>
2023-09-08 17:47:53 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	char Alphabet = 'a';
+
+	while (Alphabet <= 'z')
+	{
+		putchar(Alphabet);
+	Alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
