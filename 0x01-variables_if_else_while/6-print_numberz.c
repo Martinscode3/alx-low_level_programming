2e0c5636384814f9f84da2141d4053e1b64ddb06
Martins Okeke <martinokeke@rocketmail.com>
2023-09-08 20:06:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return o
+ */
+int main(void)
+{
+	int p;
+
+	p = 0;
+
+	while
+		(p < 0)
+		{
+			putchar(p + '0');
+			p++;
+		}
+			return (0);
+}
