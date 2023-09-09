d19edf4fb8c03fe0679d47ef9e22df2b5a52258e
Martins Okeke <martinokeke@rocketmail.com>
2023-09-08 20:21:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return o
+ */
+int main(void)
+{
+	char p;
+
+	p = 'z';
+
+	while
+		(p >= 'a')
+		{
+			putchar(p);
+			p++;
+		}
+			putchar('\n');
+			return (0);
+}
