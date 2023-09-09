72c0f7a5dfc728608f58836fc55a5671b1820bb5
Martins Okeke <martinokeke@rocketmail.com>
2023-09-08 19:43:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return o
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 10; p++)
+	{
+		printf("%d", p);
+		putchar('\n');
+	}
+		return (0);
+}
