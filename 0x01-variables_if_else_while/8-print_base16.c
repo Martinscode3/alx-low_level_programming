1256ca4a2b1dad5f51ab28d8c905a2120e599e2d
Martins Okeke <martinokeke@rocketmail.com>
2023-09-08 20:32:24 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return o
+ */
+int main(void)
+{
+	char p;
+
+	int c;
+
+	p = 'a';
+	c = 0;
+
+	while
+		(c < 10)
+		{
+			putchar(c + '0');
+			c++;
+		}
+	while
+		(p <= 'f')
+		{
+			putchar(p);
+			p++;
+		}
+			putchar('\n');
+			return (0);
+}
