06efa0160423abb6704642b12ef188134a3f0808
Martins Okeke <martinokeke@rocketmail.com>
2023-09-13 01:03:28 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,17 +2,17 @@
 /**
  * print_sign - Print the sign of a number
  * @n: The number to be checked
- * Return: 1 for positive number, -1 for negative number or zero for anything else
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 
 int print_sign(int n)
 {
-	if (n > 1)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n < 1)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
