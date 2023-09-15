5890ee5db5c75d1cc027357aa2374d0715090495
Martins Okeke <martinokeke@rocketmail.com>
2023-09-14 19:30:15 -0700

    5. Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,9 @@ void more_numbers(void)
 		for (b = 0; b < 15; b++)
 		{
 			if (b >= 10)
-			{
-				_putchar(b / 10 + '0');
-				_putchar(b % 10 + '0');
-			}
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
 		}
-				_putchar('\n');
+		_putchar('\n');
 	}
 }
